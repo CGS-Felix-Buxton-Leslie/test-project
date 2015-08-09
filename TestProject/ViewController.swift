98ffec893969ce7beebfa6ec7842236876ae291c
Felix Buxton-Leslie <39659@cgs.act.edu.au>
2015-08-10 09:14:51 +1000

    Renamed Function

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         println("Hello World")
     }
 
-    func awesomeFeature() {
+    func notSoAverageFeatureFeature() {
         
             println("This is an awesome feature")
     
